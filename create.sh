bf671a6674897718ac8cddbcfa48eb08fa75d529
David Guyomarch <davidguyomarch@gmail.com>
2018-04-05 18:17:11 +0200

    Typo

--- create.sh
+++ create.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
+
+profile=$1
+BucketName="vo-oab-bpce-marketplace-frontend-bucket"
+if [ $profile = "hicham" ]
+then
+  echo "Creating Stack in Hicham AWS Account"
+  BucketName="oab-bpce-marketplace-frontend-bucket"
+else
+  echo "Creating Stack in VO AWS Account"
+  profile="default"
+fi
 aws cloudformation create-stack \
-  --stack-name PrivateApiGateway    \
+  --stack-name BpceMarketplace \
   --template-body file://./resources.yaml \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameters \
-      ParameterKey=KeyName,ParameterValue=david_eu_central_1
+      ParameterKey=BucketName,ParameterValue=$BucketName \
+      ParameterKey=AppName,ParameterValue=BpceMarketplace \
+  --profile $profile
