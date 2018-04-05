07c66cd79d804243a4fec50bb6fa473da79eb446
David Guyomarch <davidguyomarch@gmail.com>
2018-04-05 18:18:19 +0200

    Correcting the update.sh script, clearly outdated

--- update.sh
+++ update.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
+profile=$1
+BucketName="vo-oab-bpce-marketplace-frontend-bucket"
+if [ $profile = "hicham" ]
+then
+  echo "Updating Stack in Hicham AWS Account"
+  BucketName="oab-bpce-marketplace-frontend-bucket"
+else
+  echo "Updating Stack in VO AWS Account"
+  profile="default"
+fi
 
 aws cloudformation update-stack \
-  --stack-name BpceMarketplace    \
+  --stack-name BpceMarketplace \
   --template-body file://./resources.yaml \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameters \
-      ParameterKey=KeyName,ParameterValue=david_eu_central_1
-
-  # \
-  # --profile hicham
-
-  # \
-  # --parameters \
-  #     ParameterKey=KeyName,ParameterValue=david_eu_central_1
+      ParameterKey=BucketName,ParameterValue=$BucketName \
+      ParameterKey=AppName,ParameterValue=BpceMarketplace \
+  --profile $profile
