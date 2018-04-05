f4f1eed1ef50d6275ff0e0400ca729c4ae6133a2
David Guyomarch <davidguyomarch@gmail.com>
2018-04-05 16:56:31 +0200

    Cognito Ok, Web App Client tested, not integrated in the commit

--- update.sh
+++ update.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+
+
 aws cloudformation update-stack \
-  --stack-name PrivateApiGateway    \
+  --stack-name BpceMarketplace    \
   --template-body file://./resources.yaml \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameters \
       ParameterKey=KeyName,ParameterValue=david_eu_central_1
+
+  # \
+  # --profile hicham
+
+  # \
+  # --parameters \
+  #     ParameterKey=KeyName,ParameterValue=david_eu_central_1
