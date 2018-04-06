ff619bd55efafd89188768ff67c45f96963ee606
David Guyomarch <davidguyomarch@gmail.com>
2018-04-06 16:49:11 +0200

    Adding auto deployement of config file and website

--- webapp/deploy_website.sh
+++ webapp/deploy_website.sh
@@ -11,5 +11,48 @@ else
   profile="default"
 fi
 
+UserPoolId=$(aws cloudformation describe-stacks \
+    --stack-name BpceMarketplace \
+    --query 'Stacks[0].Outputs[?OutputKey==`CognitoUserPoolName`].OutputValue' \
+    --output text)
+UserPoolArn=$(aws cloudformation describe-stacks \
+    --stack-name BpceMarketplace \
+    --query 'Stacks[0].Outputs[?OutputKey==`CognitoUserPoolArn`].OutputValue' \
+    --output text)
+IdentityPoolId=$(aws cloudformation describe-stacks \
+    --stack-name BpceMarketplace \
+    --query 'Stacks[0].Outputs[?OutputKey==`CognitoIdentityPoolId`].OutputValue' \
+    --output text)
+ClientId=$(aws cloudformation describe-stacks \
+    --stack-name BpceMarketplace \
+    --query 'Stacks[0].Outputs[?OutputKey==`CognitoIdentityPoolClientId`].OutputValue' \
+    --output text)
+
+# echo "UserPoolId $UserPoolId"
+# echo "UserPoolArn $UserPoolArn"
+# echo "IdentityPoolId $IdentityPoolId"
+# echo "ClientId $ClientId"
+
+cat > ./src/aws-exports.js <<EOF
+
+const aws_exports = {
+    Auth: {
+    // REQUIRED - Amazon Cognito Identity Pool ID
+        identityPoolId: '$IdentityPoolId',
+    // REQUIRED - Amazon Cognito Region
+        region: 'eu-central-1',
+    // OPTIONAL - Amazon Cognito User Pool ID
+        userPoolId: '$UserPoolId',
+    // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+        userPoolWebClientId: '$ClientId',
+    // OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
+        mandatorySignIn: false,
+    }
+};
+
+export default aws_exports
+
+EOF
+
 npm run build
-aws s3 sync ./dist/ s3://$BucketName
+aws s3 sync ./dist/ s3://$BucketName --profile $profile
