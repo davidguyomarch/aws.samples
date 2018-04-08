bf55fcb5acadb7a703ba8170aad0557eecce42d7
David Guyomarch <davidguyomarch@gmail.com>
2018-04-08 10:48:40 +0200

    Copying config params from create.sh script to deploy_website

--- webapp/deploy_website.sh
+++ webapp/deploy_website.sh
@@ -1,37 +1,88 @@
 #!/bin/bash
 
-profile=$1
-BucketName="vo-oab-bpce-marketplace-frontend-bucket"
-if [ $profile = "hicham" ]
-then
-  echo "Deploying Website in Hicham AWS Account"
-  BucketName="oab-bpce-marketplace-frontend-bucket"
-else
-  echo "Deploying Website in VO AWS Account"
-  profile="default"
-fi
+PROFILE="default"
+BUCKET_NAME="vo-oab-bpce-marketplace-frontend-bucket"
+STACK_NAME="ServerlessDemo"
+
+usage()
+{
+cat <<-EOF
+USAGE: usage [OPTIONS] [TEXT]
+
+ OPTIONS
+
+   -h | --help | -?      Display this help message
+
+   -p | --profile        Define the aws profile of your .aws/config file you want to use.
+                         default: default
+
+   -s | --s3BucketName   Define the name of your s3bucket
+                         default: oab-bpce-marketplace-frontend-bucket
+
+   -n | --stackname      Define the name of the stack
+                         default: ServerlessDemo
+EOF
+  return
+}
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -h | --help | -\?)
+      shift # past argument
+      usage
+      exit
+      ;;
+    -p | --profile)
+      PROFILE="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -s | --s3BucketName)
+      BUCKET_NAME="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -n | --stackname)
+      STACK_NAME="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    *)    # unknown option
+      POSITIONAL+=("$1") # save it in an array for later
+      shift # past argument
+      echo "ERROR: unknown parameter \"$POSITIONAL\""
+      usage
+      exit 1
+      ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
 
 UserPoolId=$(aws cloudformation describe-stacks \
-    --stack-name BpceMarketplace \
+    --stack-name $STACK_NAME \
     --query 'Stacks[0].Outputs[?OutputKey==`CognitoUserPoolName`].OutputValue' \
-    --output text)
+    --output text \
+    --profile $PROFILE)
 UserPoolArn=$(aws cloudformation describe-stacks \
-    --stack-name BpceMarketplace \
+    --stack-name $STACK_NAME \
     --query 'Stacks[0].Outputs[?OutputKey==`CognitoUserPoolArn`].OutputValue' \
-    --output text)
+    --output text \
+    --profile $PROFILE)
 IdentityPoolId=$(aws cloudformation describe-stacks \
-    --stack-name BpceMarketplace \
+    --stack-name $STACK_NAME \
     --query 'Stacks[0].Outputs[?OutputKey==`CognitoIdentityPoolId`].OutputValue' \
-    --output text)
+    --output text \
+    --profile $PROFILE)
 ClientId=$(aws cloudformation describe-stacks \
-    --stack-name BpceMarketplace \
+    --stack-name $STACK_NAME \
     --query 'Stacks[0].Outputs[?OutputKey==`CognitoIdentityPoolClientId`].OutputValue' \
-    --output text)
-
-# echo "UserPoolId $UserPoolId"
-# echo "UserPoolArn $UserPoolArn"
-# echo "IdentityPoolId $IdentityPoolId"
-# echo "ClientId $ClientId"
+    --output text \
+    --profile $PROFILE)
 
 cat > ./src/aws-exports.js <<EOF
 
@@ -55,4 +106,4 @@ export default aws_exports
 EOF
 
 npm run build
-aws s3 sync ./dist/ s3://$BucketName --profile $profile
+aws s3 sync ./dist/ s3://$BUCKET_NAME --profile $PROFILE
